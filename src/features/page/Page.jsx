cbfc70191c13664cf643b0e8e541493723910044
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-05-09 16:08:46 -0700

    Update Page.jsx

--- src/features/page/Page.jsx
+++ src/features/page/Page.jsx
@@ -4,40 +4,44 @@ import Footer from "./Footer";
 import {createContext, useState} from "react";
 import PageNavigation from "./PageNavigation";
 
+export const SpeechContext = createContext([]);
 export const LayoutContext = createContext([]);
 
 const Page = () => {
+    const [speech, setSpeech] = useState('')
     const [layout, setLayout] = useState('teams')
 
     return (
-        <LayoutContext.Provider value={[layout, setLayout]}>
-            <Container
-                maxWidth='sm'
-            >
-                <Box
-                    display='flex'
-                    flexDirection='column'
-                    justifyContent='space-between'
-                    height='100vh'
-                    minWidth={350}
-                    py={2}
+        <SpeechContext.Provider value={[speech, setSpeech]}>
+            <LayoutContext.Provider value={[layout, setLayout]}>
+                <Container
+                    maxWidth='sm'
                 >
-                    <Grid
-                        container
-                        direction='column'
-                        spacing = {2}
+                    <Box
+                        display='flex'
+                        flexDirection='column'
+                        justifyContent='space-between'
+                        height='100vh'
+                        minWidth={350}
+                        py={2}
                     >
-                        <Grid item>
-                            <Navbar />
+                        <Grid
+                            container
+                            direction='column'
+                            spacing = {2}
+                        >
+                            <Grid item>
+                                <Navbar />
+                            </Grid>
+                            <Grid item>
+                                <PageNavigation />
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <PageNavigation />
-                        </Grid>
-                    </Grid>
-                    <Footer />
-                </Box>
-            </Container>
-        </LayoutContext.Provider>
+                        <Footer />
+                    </Box>
+                </Container>
+            </LayoutContext.Provider>
+        </SpeechContext.Provider>
     )
 }
 
